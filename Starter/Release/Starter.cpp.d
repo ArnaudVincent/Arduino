666393ea100a6074cf92feb646fc51db1286c57b
Arnaud VINCENT <arnaud.vincent@hotmail.fr>
2018-10-08 23:01:16 +0200

    f

---
+++ Starter/Release/Starter.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\Arnaud\AppData\Local\Temp\VMBuilds\Starter\mega_atmega2560\Release\Starter.cpp.o: \
+ C:\Users\Arnaud\AppData\Local\Temp\VMBuilds\Starter\mega_atmega2560\Release\Starter.cpp \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Domotic\arduino-1.8.6\hardware\arduino\avr\variants\mega/pins_arduino.h
